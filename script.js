84a4032860c6b57f4b580c5be7c270c735dd4ce9
harshad gupta <127613271+harsheds@users.noreply.github.com>
2023-06-06 16:25:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-// Get the necessary elements from the DOM
 const generateBtn = document.getElementById("generateBtn");
-const copyBtn = document.getElementById("copyBtn");
 const passwordOutput = document.getElementById("passwordOutput");
+const copyBtn = document.getElementById("copyBtn");
 
-// Add click event listener to the generate button
-generateBtn.addEventListener("click", function() {
-  const passwordLength = document.getElementById("passwordLength").value;
-  const generatedPassword = generatePassword(passwordLength);
-  passwordOutput.value = generatedPassword;
-});
+generateBtn.addEventListener("click", generatePassword);
+copyBtn.addEventListener("click", copyPassword);
 
-// Add click event listener to the copy button
-copyBtn.addEventListener("click", function() {
-  passwordOutput.select();
-  document.execCommand("copy");
-});
+function generatePassword() {
+    const length = document.getElementById("passwordLength").value;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+    let password = "";
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    
+    passwordOutput.value = password;
+}
 
-// Function to generate a random password
-function generatePassword(length) {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
-  let password = "";
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters.charAt(randomIndex);
-  }
-  return password;
+function copyPassword() {
+    passwordOutput.select();
+    passwordOutput.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Password copied to clipboard!");
 }
