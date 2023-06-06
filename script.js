a63ac06a7519517b5563ecf535e1b87bf5997d7c
harshad gupta <127613271+harsheds@users.noreply.github.com>
2023-06-06 16:09:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-function generatePassword() {
-    var length = parseInt(document.getElementById('passwordLength').value);
-    var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+';
-
-    var password = '';
-    for (var i = 0; i < length; i++) {
-        var randomIndex = Math.floor(Math.random() * characters.length);
-        password += characters.charAt(randomIndex);
-    }
-
-    return password;
-}
-
-// external.js
-
-// Get the copy button and password output element
+// Get the necessary elements from the DOM
+const generateBtn = document.getElementById("generateBtn");
 const copyBtn = document.getElementById("copyBtn");
 const passwordOutput = document.getElementById("passwordOutput");
 
+// Add click event listener to the generate button
+generateBtn.addEventListener("click", function() {
+  const passwordLength = document.getElementById("passwordLength").value;
+  const generatedPassword = generatePassword(passwordLength);
+  passwordOutput.value = generatedPassword;
+});
+
 // Add click event listener to the copy button
 copyBtn.addEventListener("click", function() {
-    passwordOutput.select();
-    document.execCommand("copy");
+  passwordOutput.select();
+  document.execCommand("copy");
 });
 
-
-document.getElementById('generateBtn').addEventListener('click', function () {
-    var passwordOutput = document.getElementById('passwordOutput');
-    passwordOutput.value = generatePassword();
-});
-
-document.getElementById('copyBtn').addEventListener('click', copyToClipboard);
+// Function to generate a random password
+function generatePassword(length) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+  return password;
+}
